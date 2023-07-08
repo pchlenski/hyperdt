3a6cef87094c63b06272aa42462cc9d2fad43883
pchlenski <pac@cs.columbia.edu>
2023-07-08 13:38:30 -0400

    Random forest update

---
+++ src/forest.py
@@ -0,0 +1,38 @@
+"""Hyperbolic random forest"""
+
+import numpy as np
+from scipy import stats
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.ensemble import RandomForestClassifier
+
+from .tree import HyperbolicDecisionTreeClassifier
+
+
+class HyperbolicRandomForestClassifier:
+    def __init__(
+        self, n_estimators=100, max_depth=3, min_samples=2, hyperbolic=True
+    ):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.min_samples = min_samples
+        self.hyperbolic = hyperbolic
+        self.trees = [
+            HyperbolicDecisionTreeClassifier(max_depth, min_samples, hyperbolic)
+            for _ in range(n_estimators)
+        ]
+
+    def _generate_subsample(self, X, y):
+        n_samples = X.shape[0]
+        indices = np.random.choice(n_samples, n_samples, replace=True)
+        return X[indices], y[indices]
+
+    def fit(self, X, y):
+        for tree in self.trees:
+            X_sample, y_sample = self._generate_subsample(X, y)
+            tree.fit(X_sample, y_sample)
+        return self
+
+    def predict(self, X):
+        predictions = np.array([tree.predict(X) for tree in self.trees])
+        return np.squeeze(stats.mode(predictions, axis=0).mode)
