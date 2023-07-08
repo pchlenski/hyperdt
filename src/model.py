d2a2c8b5bd678eb46d09feeec3681f9ae983be45
pchlenski <pac@cs.columbia.edu>
2023-07-08 12:47:04 -0400

    Finally got this code working!

--- src/model.py
+++ src/model.py
@@ -9,257 +9,273 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from itertools import combinations
 
 
-def _gini(x):
-    """Gini impurity: 1 - sum(p_i^2)"""
-    if x.dtype == int:
-        counts = np.bincount(x, minlength=2)
-    else:
-        _, counts = np.unique(x, return_counts=True)
-    p = counts / len(x)
-    return 1.0 - np.sum(p ** 2)
+class HyperbolicDecisionNode:
+    def __init__(
+        self,
+        leaf=False,
+        id=None,
+        feature=None,
+        theta=None,
+        left=None,
+        right=None,
+        value=None,
+    ):
+        """Init node"""
+        self.leaf = leaf
+        self.id = id
+        self.feature = feature
+        self.theta = theta
+        self.left = left
+        self.right = right
+        self.value = value
+
+
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
 
 
 class HyperbolicDecisionNode:
     def __init__(
-        self,
-        X,
-        y,
-        depth,
-        dim=None,
-        theta=None,
-        leaf=False,
-        parent=None,
-        id=None,
+        self, leaf=False, value=None, feature=None, theta=None, id=None
     ):
-        self.X = X
-        self.y = y
-        self.depth = depth
-        self.dim = dim
-        self.theta = theta
         self.leaf = leaf
-        self.parent = parent
+        self.value = value
+        self.feature = feature
+        self.theta = theta
+        self.id = id
         self.left = None
         self.right = None
-        self.id = id
-
-        # If this is a leaf node, compute and store its prediction
-        if self.leaf and len(y) > 0:
-            classes, counts = np.unique(y, return_counts=True)
-            self.prediction = classes[np.argmax(counts)]  # most common class
-            self.probs = dict(zip(classes, counts / len(y)))
-        else:
-            self.prediction = None
-            self.probs = None
 
 
 class HyperbolicDecisionTreeClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self, max_depth=3, min_samples=2, hyperbolic=True):
-        """Init classifier"""
         self.max_depth = max_depth
         self.min_samples = min_samples
         self.hyperbolic = hyperbolic
-
-        # Initialize tree
         self.tree = None
 
+    def _normal(self, dim, theta):
+        v = np.zeros(self.ndim)
+        v[0], v[dim] = np.sin(theta), np.cos(theta)
+        return v
+
+    def _gini(self, y):
+        _, counts = np.unique(y, return_counts=True)
+        probs = counts / len(y)
+        return 1 - np.sum(probs ** 2)
+
+    def _information_gain(self, left, right, y):
+        n = len(y)
+        n_l, n_r = len(y[left]), len(y[right])
+        if np.min([n_l, n_r]) < self.min_samples:
+            return -1
+        parent_loss = self._gini(y)
+        child_loss = (
+            n_l * self._gini(y[left]) + n_r * self._gini(y[right])
+        ) / n
+        return parent_loss - child_loss
+
     def _get_split(self, X, dim, theta):
-        """
-        Get indices to left and right of decision hyperplane at angle theta
-        """
-
-        # Get left and right nodes
-        # Note that this approach does not include a bias term. We do not
-        # allow bias terms in the hyperboloid case, since they do not intersect
-        # the manifold at geodesics.
-
         if self.hyperbolic:
-            # Normal vector for decision hyperplane (passing through origin)
-            normal = np.zeros(X.shape[1])
-            normal[0], normal[dim] = np.sin(theta), np.cos(theta)
-            prods = X @ normal
-
+            prods = X @ self._normal(dim, theta)
+            return prods < 0.0, prods >= 0.0
         else:
-            # Normal vector for decision hyperplane (with bias)
-            normal = np.zeros(X.shape[1])
-            normal[dim] = 1.0
-            prods = X @ normal - theta
+            return X[:, dim] < theta, X[:, dim] >= theta
 
-        left = prods < 0.0
-        right = prods >= 0.0
-        print("LEFT", left)
-        return left, right
+    def _get_candidates(self, X, dim):
+        X[:, dim][X[:, dim] == 0.0] = 1e-6
+        thetas = np.arctan(X[:, 0] / X[:, dim])
+        thetas = np.unique(np.sort(thetas))
+        return (thetas[1:] + thetas[:-1]) / 2.0
 
-    def _get_impurity(self, left, right, y, leaf=False):
-        """Get impurity of a split"""
-        y_left, y_right = y[left], y[right]
-        min_samples = np.min([len(y_left), len(y_right)])
-        print(f"min_samples: {min_samples}")
-        if min_samples < self.min_samples:
-            return np.inf
-        else:
-            return _gini(y_left) + _gini(y_right)
-
-    def _get_best_theta_dim(self, X, y, dim):
-        """
-        Find best split dimension for a given dimension. This is done by
-        finding argmin_t of a normal vector (t, 0, ..., 1, ..., 0), where x_0 is
-        the time-like coordinate and x_dim is our split dimension, with respect
-        to the impurity of the resulting nodes.
-        """
-
-        # Input validation
-        assert dim > 0, "dim must be greater than 0"
-        assert dim < X.shape[1], "dim must be less than X.shape[1]"
-        assert len(y) == len(X), "X and y must have the same length"
-        dim = int(dim)
-
-        # First, compute tangents along (x_0, x_dim) plane, then angle:
-        X[:, dim][X[:, dim] == 0.0] = 1e-6  # Avoid division by zero
-        tans = X[:, 0] / X[:, dim]
-        tans[tans > 1e6] = 1e6  # Avoid overflow
-        tans[tans < -1e6] = -1e6  # Avoid overflow
-        thetas = np.arctan(tans)
-        thetas = np.sort(thetas)
-        thetas = np.unique(thetas)  # Remove duplicates)
-
-        # Candidates are midpoints between thetas
-        # candidates = (thetas[1:] + thetas[:-1]) / 2.0
-        # OVERRIDE
-        if self.hyperbolic:
-            candidates = np.linspace(0, 2 * np.pi, 1000)
-        else:
-            candidates = np.linspace(np.min(X[:, dim]), np.max(X[:, dim]), 1000)
-        print(f"Got {len(candidates)} candidates for dim {dim}")
-        print(
-            f"Max: {np.max(candidates)/np.pi:.3f}*pi, min: {np.min(candidates)/np.pi:.3f}*pi"
-        )
-
-        # Get best split
-        best_theta = None
-        best_score = np.inf
-        for theta in candidates:
-            left, right = self._get_split(X=X, dim=dim, theta=theta)
-            score = self._get_impurity(left, right, y)
-            if score < best_score:
-                best_theta = theta
-                best_score = score
-
-        return best_theta, best_score
-
-    def get_best_dim(self, X, y):
-        """Find best split dimension for a given node"""
-        ndim = X.shape[1]
-        dim_thetas_scores = [
-            self._get_best_theta_dim(X, y, dim) for dim in range(1, ndim)
-        ]
-        print("Got dim thetas scores")
-        dim_thetas = [theta for theta, _ in dim_thetas_scores]
-        dim_scores = [score for _, score in dim_thetas_scores]
-        best_idx = np.argmin(dim_scores)
-        print("Finishing get_best_dim")
-        return best_idx, dim_thetas[best_idx], dim_scores[best_idx]
-
-    def _fit_node(self, X, y, depth, parent=None, id="ROOT"):
-        """
-        Recursively fit nodes to the data
-        """
-
-        print(f"Fitting node at depth {depth}")
-
-        # Check for stopping conditions:
+    def _fit_node(self, X, y, depth, id="ROOT"):
         if (
-            (depth == self.max_depth)
-            or (len(y) < self.min_samples)
-            or (len(set(y)) == 1)
+            depth == self.max_depth
+            or len(y) <= self.min_samples
+            or len(set(y)) == 1
         ):
             return HyperbolicDecisionNode(
-                X, y, depth, leaf=True, parent=parent, id=id
+                leaf=True, value=self._most_common_label(y), id=id
+            )
+        # Loop through all possible splits:
+        best_dim, best_theta, best_score = None, None, -1
+        for dim in self.dims:
+            for theta in self._get_candidates(X=X, dim=dim):
+                left, right = self._get_split(X=X, dim=dim, theta=theta)
+                score = self._information_gain(left, right, y)
+                if score > best_score:
+                    best_dim, best_theta, best_score = dim, theta, score
+
+        # Contingency for no split found:
+        if best_score == -1:
+            return HyperbolicDecisionNode(
+                leaf=True, value=self._most_common_label(y), id=id
             )
 
-        # Find the best split for the current node
-        best_dim, best_theta, best_score = self.get_best_dim(X, y)
-
-        # Initialize current node
-        node = HyperbolicDecisionNode(
-            X=X,
-            y=y,
-            depth=depth,
-            dim=best_dim,
-            theta=best_theta,
-            leaf=False,
-            parent=parent,
-            id=id,
-        )
-
-        # Split the data into left and right partitions
-        left, right = self._get_split(X=X, dim=best_dim, theta=best_theta)
-        print(f"Split data into {len(X[left])} and {len(X[right])}")
-
-        # Recursive calls to _fit_node for left and right child nodes
-        node.left = self._fit_node(
-            X=X[left], y=y[left], depth=depth + 1, parent=node, id=f"{id}_L"
-        )
-        node.right = self._fit_node(
-            X=X[right], y=y[right], depth=depth + 1, parent=node, id=f"{id}_R"
-        )
-
-        # Remove X and y from parent node to save memory
+        # Populate:
+        node = HyperbolicDecisionNode(feature=best_dim, theta=best_theta, id=id)
+        l, r = self._get_split(X=X, dim=best_dim, theta=best_theta)
+        d = depth + 1
+        node.left = self._fit_node(X=X[l], y=y[l], depth=d, id=f"{id}_L")
+        node.right = self._fit_node(X=X[r], y=y[r], depth=d, id=f"{id}_R")
         node.X = None
         node.y = None
-
         return node
 
-    def _validate(self, X):
-        """Ensure data lies on hyperboloid"""
-        if np.allclose(X[:, 0], np.sqrt(1 + np.sum(X[:, 1:] ** 2, axis=1))):
-            return True
-        else:
-            raise ValueError("Data must lie on hyperboloid")
+    def _most_common_label(self, y):
+        _, counts = np.unique(y, return_counts=True)
+        return np.argmax(counts) if len(counts) > 0 else None
 
     def fit(self, X, y):
-        """Fit hyperbolic decision tree to training data"""
-        # Initialize tree by fitting the root node to the data
-        self._validate(X)
-
+        self.ndim = X.shape[1]
+        self.dims = range(1, self.ndim) if self.hyperbolic else range(self.ndim)
         self.tree = self._fit_node(X=X, y=y, depth=0)
-
         return self
 
     def _traverse(self, x, node):
-        """
-        Traverse the tree for a single instance.
-        """
-
-        # If this is a leaf node, return its prediction
         if node.leaf:
             return node
-
-        # Compute dot product of instance and normal vector
-        normal = np.zeros(len(x))
-        normal[0], normal[node.dim] = np.sin(node.theta), np.cos(node.theta)
-        dot_product = x @ normal
-
-        # Traverse to the left or right child node
-        if dot_product < 0.0:
-            return self._traverse(x, node.left)
+        if self.hyperbolic:
+            v = self._normal(node.feature, node.theta)
+            return (
+                self._traverse(x, node.left)
+                if x @ v < 0.0
+                else self._traverse(x, node.right)
+            )
         else:
-            return self._traverse(x, node.right)
+            return (
+                self._traverse(x, node.left)
+                if x[node.feature] < node.theta
+                else self._traverse(x, node.right)
+            )
 
     def predict(self, X):
-        """
-        Predict class labels for samples in X.
-        """
-
-        # Initialize array for predictions
-        y_pred = np.empty(len(X))
-
-        # Traverse tree for each instance
-        for i, x in enumerate(X):
-            leaf = self._traverse(x, self.tree)
-            y_pred[i] = leaf.prediction
-
-        return y_pred
+        return np.array([self._traverse(x, self.tree).value for x in X])
 
     def predict_probs(self, X):
         """Predict class probabilities for samples in X"""
         raise NotImplementedError
+
+
+def gini(y):
+    _, counts = np.unique(y, return_counts=True)
+    probs = counts / len(y)
+    return 1 - np.sum(probs ** 2)
+
+
+class DecisionNode:
+    def __init__(
+        self, feature=None, threshold=None, left=None, right=None, value=None
+    ):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+
+class DecisionTree:
+    def __init__(self, min_samples=2, max_depth=100, hyperbolic=True):
+        self.min_samples_split = min_samples
+        self.max_depth = max_depth
+        self.root = None
+        self.hyperbolic = hyperbolic
+
+    def _build_tree(self, X, y, depth=0):
+        n_samples, n_features = X.shape
+        if self.hyperbolic:
+            n_features = n_features - 1  # First col is timelike
+        n_labels = len(np.unique(y))
+
+        # stopping condition
+        if (
+            depth >= self.max_depth
+            or n_labels == 1
+            or n_samples < self.min_samples_split
+        ):
+            leaf_value = self._most_common_label(y)
+            return DecisionNode(value=leaf_value)
+
+        best_gain = -1
+        split_idx, split_threshold = None, None
+
+        for feature_index in range(n_features):
+            X_column = X[:, feature_index]
+            if self.hyperbolic:
+                tans = X[:, 0] / X[:, feature_index]
+                tans[tans > 1e6] = 1e6
+                tans[tans < -1e6] = -1e6
+                tans[tans == 0] = 1e-6
+                tans = np.unique(tans)
+                thresholds = np.sort(np.arctan(tans))[1:-1]
+            else:
+                thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                gain = self._information_gain(y, X, feature_index, threshold)
+
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feature_index
+                    split_threshold = threshold
+
+        left_idxs = X[:, split_idx] < split_threshold
+        right_idxs = X[:, split_idx] >= split_threshold
+        left = self._build_tree(X[left_idxs, :], y[left_idxs], depth + 1)
+        right = self._build_tree(X[right_idxs, :], y[right_idxs], depth + 1)
+        return DecisionNode(
+            feature=split_idx, threshold=split_threshold, left=left, right=right
+        )
+
+    def _information_gain(self, y, X, column, split_threshold):
+        # parent loss
+        parent_loss = gini(y)
+
+        # generate split
+        if self.hyperbolic:
+            theta = split_threshold  # For clarity
+            v = np.zeros(X.shape[1])
+            v[0], v[column] = np.sin(theta), np.cos(theta)
+            prods = np.dot(X, v)
+            left_idxs = prods < 0
+            right_idxs = prods >= 0
+
+        else:
+            X_column = X[:, column]
+            left_idxs = X_column < split_threshold
+            right_idxs = X_column >= split_threshold
+        if len(y[left_idxs]) == 0 or len(y[right_idxs]) == 0:
+            return 0
+
+        # compute the weighted avg. of the loss for the children
+        n = len(y)
+        n_l, n_r = len(y[left_idxs]), len(y[right_idxs])
+        e_l, e_r = gini(y[left_idxs]), gini(y[right_idxs])
+        child_loss = (n_l / n) * e_l + (n_r / n) * e_r
+
+        # information gain is difference in loss before vs. after split
+        ig = parent_loss - child_loss
+        return ig
+
+    def _most_common_label(self, y):
+        y = np.array(y).astype(int)
+        return np.bincount(y).argmax() if len(y) > 0 else None
+
+    def fit(self, X, y):
+        self.root = self._build_tree(X, y)
+
+    def _predict(self, inputs, tree=None):
+        if tree is None:
+            tree = self.root
+
+        if tree.value is not None:
+            return tree.value
+
+        feature_value = inputs[tree.feature]
+
+        if feature_value < tree.threshold:
+            return self._predict(inputs, tree.left)
+
+        return self._predict(inputs, tree.right)
+
+    def predict(self, X):
+        return np.array([self._predict(inputs) for inputs in X])
