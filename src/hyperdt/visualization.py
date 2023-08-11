d8bb8a20e7947a1344098c12ba5a3b73e7965826
pchlenski <pac@cs.columbia.edu>
2023-08-11 18:03:41 -0400

    Fix projection issue

--- src/hyperdt/visualization.py
+++ src/hyperdt/visualization.py
@@ -32,9 +32,13 @@ def _find_intersection(D, theta, t, t_dim=1, n_dim=3):
 
     # return x_0_solution, x_D_solution, t
     out_vec = np.zeros(n_dim)
-    out_vec[0] = -x0_solution
     out_vec[t_dim] = t
-    out_vec[D] = -xD_solution
+    if theta < 0:
+        out_vec[0] = x0_solution
+        out_vec[D] = xD_solution
+    else:
+        out_vec[0] = -x0_solution
+        out_vec[D] = -xD_solution
 
     return out_vec
 
@@ -66,7 +70,7 @@ def _get_geodesic(
 
 
 def plot_boundary(
-    hdt,
+    hdt_node,
     X=None,
     y=None,
     t_dim=None,
@@ -76,20 +80,20 @@ def plot_boundary(
 ):
     """Plot decision boundaries of a hyperbolic decision tree"""
     # Get decision boundary parameters
-    boundary_dim = hdt.tree.feature
-    boundary_theta = hdt.tree.theta
+    boundary_dim = hdt_node.feature
+    boundary_theta = hdt_node.theta
 
-    # Set t_dim: last dimension not equal to boundary_dim
+    # Set t_dim: we assume total number of dims is 3
     if t_dim is None:
-        dims = np.arange(hdt.ndim)
-        dims = dims[dims != boundary_dim]
-        t_dim = dims[-1]
-        print(f"t_dim set to {t_dim}")
+        dims = [0, 1, 2]
+        dims.remove(boundary_dim)
+        dims.remove(timelike_dim)
+        t_dim = dims[0]
 
     # Get geodesics; project
     geodesic_points = _get_geodesic(
-        boundary_dim,
-        boundary_theta,
+        dim=boundary_dim,
+        theta=boundary_theta,
         geometry=geometry,
         t_dim=t_dim,
         timelike_dim=timelike_dim,
@@ -99,8 +103,36 @@ def plot_boundary(
     if ax is None:
         fig, ax = plt.subplots(figsize=(10, 10))
 
+    # Verify geodesics lie inside unit circle:
+    if np.all(np.linalg.norm(geodesic_points, axis=1) <= 1):
+        ax.plot(geodesic_points[:, 0], geodesic_points[:, 1], c="red")
+    else:
+        print(
+            f"Geodesic points lie outside unit circle:\t{boundary_dim} {boundary_theta/np.pi:.3f}*pi {t_dim}"
+        )
+
+    # Set axis limits
+    ax.set_xlim([-1.1, 1.1])
+    ax.set_ylim([-1.1, 1.1])
+
+    return ax
+
+
+def _plot_tree_recursive(node, **kwargs):
+    if node.value is not None:  # Leaf case
+        return kwargs["ax"]
+    else:
+        ax = plot_boundary(node, **kwargs)
+        ax = _plot_tree_recursive(node.left, **kwargs)
+        ax = _plot_tree_recursive(node.right, **kwargs)
+        return ax
+
+
+def plot_tree(hdt, X=None, y=None, geometry="poincare", timelike_dim=0):
+    fig, ax = plt.subplots(figsize=(10, 10))
+
     # Draw unit circle
-    _x = np.linspace(-1, 1, 100)
+    _x = np.linspace(-1, 1, 1000)
     _y = np.sqrt(1 - _x ** 2)
     ax.plot(_x, _y, c="black")
     ax.plot(_x, -_y, c="black")
@@ -112,11 +144,6 @@ def plot_boundary(
         )
         ax.scatter(X[:, 0], X[:, 1], c=y)
 
-    # Plot geodesics
-    ax.plot(geodesic_points[:, 0], geodesic_points[:, 1], c="red")
-
-    # Set axis limits
-    ax.set_xlim([-1.1, 1.1])
-    ax.set_ylim([-1.1, 1.1])
-
-    return ax
+    return _plot_tree_recursive(
+        hdt.tree, ax=ax, geometry=geometry, timelike_dim=0
+    )
