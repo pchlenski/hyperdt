0edbc2ef41f01bf597d499a9144ea95d0823bcba
pchlenski <pac@cs.columbia.edu>
2023-11-10 17:16:29 -0500

    Archival change: mostly midpoint and stetup stuff

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hyperdt',
-    version='0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    name="hyperdt",
+    version="0.1",
+    packages=find_packages(where="src/hyperdt"),
+    package_dir={"": "src"},
 )
